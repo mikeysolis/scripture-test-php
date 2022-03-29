fd717786b8f216803c25a85bfa88b818e4e10549
Mike <mikeyrsolis@gmail.com>
2022-03-28 20:49:34 -1000

    Removed index.php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,17 @@
 <?php
 
-// Forward Vercel requests to normal index.php
-require __DIR__ . '/../index.php';
+$hello = 'Hello World!';
+
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Scripture API Test</title>
+</head>
+<body>
+    <h1>{$hello}</h1>
+</body>
+</html>
