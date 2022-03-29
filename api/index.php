c272d8c5c866cf739d32e03cda94b75ed5edf3f4
Mike <mikeyrsolis@gmail.com>
2022-03-28 21:01:47 -1000

    Second attempt

--- api/index.php
+++ api/index.php
@@ -1,17 +1,4 @@
 <?php
 
-$hello = 'Hello World!';
-
-?>
-
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Scripture API Test</title>
-</head>
-<body>
-    <h1>{$hello}</h1>
-</body>
-</html>
+header('content-type: application/json');
+echo json_encode(['time' => time(), 'date' => date('d.m.Y'), 'tech' => 'Vercel']);
